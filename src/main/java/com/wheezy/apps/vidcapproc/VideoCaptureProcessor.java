29c1f7b091ba6a3fd225dbafeb1cac15179edcac
Skylar Scaling <skylar.scaling@gmail.com>
2019-11-15 20:29:40 -0700

    Initial commit

---
+++ src/main/java/com/wheezy/apps/vidcapproc/VideoCaptureProcessor.java
@@ -0,0 +1,13 @@
+package com.wheezy.apps.vidcapproc;
+
+/**
+ * Hello world!
+ *
+ */
+public class VideoCaptureProcessor 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/wheezy/apps/vidcapproc/VideoCaptureProcessorTest.java
@@ -0,0 +1,17 @@
+package com.wheezy.apps.vidcapproc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class VideoCaptureProcessorTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
