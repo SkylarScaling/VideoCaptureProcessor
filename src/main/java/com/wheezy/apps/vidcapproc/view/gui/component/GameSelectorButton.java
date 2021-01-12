8e169f857442c26136868fef955e7c52c23b70dc
Skylar Scaling <skylar.scaling@gmail.com>
2021-01-11 21:36:21 -0700

    Format

--- src/main/java/com/wheezy/apps/vidcapproc/view/gui/component/GameSelectorButton.java
+++ src/main/java/com/wheezy/apps/vidcapproc/view/gui/component/GameSelectorButton.java
@@ -18,90 +18,89 @@ import javax.swing.border.MatteBorder;
 import com.wheezy.apps.vidcapproc.data.Game;
 import com.wheezy.apps.vidcapproc.view.gui.GameEditor;
 
-@SuppressWarnings("serial")
 public class GameSelectorButton extends JButton
 {
-    private static Dimension gameButtonDimension = new Dimension(74, 74);
-    private static Color selectedBorderColor = Color.RED;
-    private static MatteBorder selectedBorder =
-            BorderFactory.createMatteBorder(4, 4, 4, 4, selectedBorderColor);
+  private static final long serialVersionUID = 1L;
+  
+  private static Dimension gameButtonDimension = new Dimension(74, 74);
+  private static Color selectedBorderColor = Color.RED;
+  private static MatteBorder selectedBorder = BorderFactory.createMatteBorder(4, 4, 4, 4, selectedBorderColor);
 
-    private boolean selected = false;
-    private Game game;
-    private JPopupMenu rightClickMenu = new JPopupMenu();
+  private boolean selected = false;
+  private Game game;
+  private JPopupMenu rightClickMenu = new JPopupMenu();
 
-    public GameSelectorButton(Game game)
+  public GameSelectorButton(Game game)
+  {
+    super();
+
+    this.game = game;
+
+    JMenuItem editGameMenuItem = new JMenuItem("Edit...");
+    editGameMenuItem.addActionListener(new ActionListener()
     {
-        super();
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        GameEditor gameEditor = new GameEditor(GameSelectorButton.this.game);
+        gameEditor.setLocationRelativeTo(GameSelectorButton.this);
+        gameEditor.setVisible(true);
+      }
+    });
+    editGameMenuItem.setFont(new Font("Arial", Font.BOLD, 11));
+    rightClickMenu.add(editGameMenuItem);
 
-        this.game = game;
-
-        JMenuItem editGameMenuItem = new JMenuItem("Edit...");
-        editGameMenuItem.addActionListener(new ActionListener()
+    addMouseListener(new MouseAdapter()
+    {
+      @Override
+      public void mousePressed(MouseEvent e)
+      {
+        if (e.isPopupTrigger())
         {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                GameEditor gameEditor =
-                        new GameEditor(GameSelectorButton.this.game);
-                gameEditor.setLocationRelativeTo(GameSelectorButton.this);
-                gameEditor.setVisible(true);
-            }
-        });
-        editGameMenuItem.setFont(new Font("Arial", Font.BOLD, 11));
-        rightClickMenu.add(editGameMenuItem);
+          showPopupMenu(e);
+        }
+      }
 
-        addMouseListener(new MouseAdapter()
+      @Override
+      public void mouseReleased(MouseEvent e)
+      {
+        if (e.isPopupTrigger())
         {
-            @Override
-            public void mousePressed(MouseEvent e)
-            {
-                if (e.isPopupTrigger())
-                {
-                    showPopupMenu(e);
-                }
-            }
+          showPopupMenu(e);
+        }
+      }
 
-            @Override
-            public void mouseReleased(MouseEvent e)
-            {
-                if (e.isPopupTrigger())
-                {
-                    showPopupMenu(e);
-                }
-            }
+      private void showPopupMenu(MouseEvent e)
+      {
+        rightClickMenu.show(e.getComponent(), e.getX(), e.getY());
+      }
+    });
+    this.setIcon(new ImageIcon(game.getIconImageFilepath()));
+    this.game = game;
+    this.setToolTipText(game.getGameTitle());
+    this.setPreferredSize(gameButtonDimension);
+  }
 
-            private void showPopupMenu(MouseEvent e)
-            {
-                rightClickMenu.show(e.getComponent(), e.getX(), e.getY());
-            }
-        });
-        this.setIcon(new ImageIcon(game.getIconImageFilepath()));
-        this.game = game;
-        this.setToolTipText(game.getGameTitle());
-        this.setPreferredSize(gameButtonDimension);
-    }
+  public String getGameTitle()
+  {
+    return game.getGameTitle();
+  }
 
-    public String getGameTitle()
-    {
-        return game.getGameTitle();
-    }
+  public void selected()
+  {
+    setBorder(selectedBorder);
+    selected = true;
+  }
 
-    public void selected()
-    {
-        setBorder(selectedBorder);
-        selected = true;
-    }
+  @Override
+  public boolean isSelected()
+  {
+    return selected;
+  }
 
-    @Override
-    public boolean isSelected()
-    {
-        return selected;
-    }
-
-    public void unselected()
-    {
-        setBorder(null);
-        selected = false;
-    }
+  public void unselected()
+  {
+    setBorder(null);
+    selected = false;
+  }
 }
